fbc969a9a5c894f69973af28865e0ba9ee279b3d
nathanieluriri <uririnathaniel@gmail.com>
2025-10-20 00:40:02 +0100

    automated commit

--- api/v1/logs.py
+++ api/v1/logs.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, HTTPException, Query, status, Path,Depends,Body
 from security.auth import accessTokenOut,verify_agent_token,verify_client_token,verify_admin_token
 from typing import List
 from datetime import datetime,timedelta
+from bson import ObjectId
 from core.scheduler import scheduler
 from schemas.response_schema import APIResponse
 from schemas.logs import (
@@ -21,7 +22,9 @@ from services.logs_service import (
     retrieve_logss_that_involve_agent_and_a_particular_job,
     retrieve_logs_by_logs_id_and_agent_id,
 )
-
+from services.jobs_service import(
+    get_jobs
+)
 router = APIRouter(prefix="/logss", tags=["Logss"])
 
 @router.get("/agent/list/{job_id}", response_model=APIResponse[List[LogsOut]])
@@ -31,11 +34,14 @@ async def list_logss(job_id:str,token: accessTokenOut = Depends(verify_agent_tok
     return APIResponse(status_code=200, data=items, detail="Fetched successfully")
 
 @router.get("/client/list/{job_id}", response_model=APIResponse[List[LogsOut]])
-async def list_logss(job_id:str,token: accessTokenOut = Depends(verify_client_token)):
-    # TODO: ADD A VERIFICATION TO KNOW IF THIS USER IS ALLOWED TO VIEW THIS LOGS
-    items = await retrieve_logss()
-    return APIResponse(status_code=200, data=items, detail="Fetched successfully")
-
+async def list_logss(job_id:str,token: accessTokenOut = Depends(verify_client_token),start:int=0,stop:int=100):
+  
+    Job =await get_jobs(filter_dict={"client_id":token.userId,"_id":ObjectId(job_id)})
+    if Job !=None:
+        items = await retrieve_logss(start=start,stop=stop)
+        return APIResponse(status_code=200, data=items, detail="Fetched successfully")
+    else: 
+        raise HTTPException(status_code=403, detail="User didn't create job so you can't view it")
 
 @router.get("/agent/view", response_model=APIResponse[LogsOut])
 async def get_my_logss(id: str = Query(..., description="logs ID to fetch specific item"), token: accessTokenOut = Depends(verify_agent_token) ):
@@ -44,42 +50,64 @@ async def get_my_logss(id: str = Query(..., description="logs ID to fetch specif
     return APIResponse(status_code=200, data=items, detail="logss items fetched")
 
 @router.get("/client/view",response_model_exclude=None, response_model=APIResponse[LogsOut])
-async def get_my_logss(id: str = Query(..., description="logs ID to fetch specific item"), token: accessTokenOut = Depends(verify_client_token) ):
-    # TODO: ADD A VERIFICATION TO KNOW IF THIS USER IS ALLOWED TO VIEW THIS LOGS
-    items = await retrieve_logs_by_logs_id(id=id)
-    return APIResponse(status_code=200, data=items, detail="logss items fetched")
+async def get_my_logss(job_id: str = Query(..., description="logs ID to fetch specific item"), token: accessTokenOut = Depends(verify_client_token) ):
+    
+    Job =await get_jobs(filter_dict={"client_id":token.userId,"_id":ObjectId(job_id)})
+    if Job !=None:
+        items = await retrieve_logs_by_logs_id(id=id)
+        return APIResponse(status_code=200, data=items, detail="logss items fetched")
+    else: 
+        raise HTTPException(status_code=403, detail="User didn't create job so you can't view it")
 
 @router.patch("/approve/{log_id}", response_model=APIResponse[LogsOut])
 async def client_endpoint_to_approve_logss(log_id: str, token: accessTokenOut = Depends(verify_client_token) ):
-    # TODO: ADD VERIFICATION TO KNOW IF THIS CLIENT CAN APPROVE THE LOG
     try:
-        log = await retrieve_logs_by_logs_id(id=log_id)
+            log = await retrieve_logs_by_logs_id(id=log_id)
     except Exception as e:
-        raise HTTPException(status_code=500,detail=f"{e}")
-    if (log.client_approved==False) and (log.rejection_reason==None):
-        update_data = LogsUpdate(client_approved=True)
+            raise HTTPException(status_code=500,detail=f"{e}")
+    
+    Job =await get_jobs(filter_dict={"client_id":token.userId,"_id":ObjectId(log.job_id)})
+    if Job !=None:
         
-        new_logs= await update_logs_by_id(logs_id=log_id,logs_data=update_data)
+        if (log.client_approved==False) and (log.rejection_reason==None):
+            update_data = LogsUpdate(client_approved=True)
+            
+            new_logs= await update_logs_by_id(logs_id=log_id,logs_data=update_data)
+            
+            return APIResponse(status_code=200,data=new_logs,detail="Successfully  approved logs")
         
-        return APIResponse(status_code=200,data=new_logs,detail="Successfully  approved logs")
-    elif(log.client_approved==True) and (log.rejection_reason==None): raise HTTPException(status_code=409,detail="Log already approved")
-    else: raise HTTPException(status_code=400,detail="Bad request")
+        elif(log.client_approved==True) and (log.rejection_reason==None): 
+            raise HTTPException(status_code=409,detail="Log already approved")
+        else: 
+            raise HTTPException(status_code=400,detail="Bad request")
+    else: 
+        raise HTTPException(status_code=403, detail="User didn't create job so you can't view it")
 
 @router.patch("/reject/{log_id}", response_model=APIResponse[LogsOut])
 async def client_endpoint_to_reject_logss(log_id: str,log_rejection:LogReject, token: accessTokenOut = Depends(verify_client_token) ):
-    # TODO: ADD VERIFICATION TO KNOW IF THIS CLIENT CAN REJECT THE LOG
+   
     try:
         log = await retrieve_logs_by_logs_id(id=log_id)
     except Exception as e:
         raise HTTPException(status_code=500,detail=f"{e}")
-    if (log.client_approved==False) and (log.rejection_reason==None):
-        update_data = LogsUpdate(client_approved=False,rejection_reason=log_rejection.rejection_reason)
-        new_logs= await update_logs_by_id(logs_id=log_id,logs_data=update_data)
-        run_time = datetime.now()+timedelta(days=7)
-        scheduler.add_job(remove_logs,"date",run_date=run_time,args=[log_id],misfire_grace_time=31536000)
-        return APIResponse(status_code=200,data=new_logs,detail="Successfully  approved logs")
-    elif(log.client_approved==False) and (log.rejection_reason!=None): raise HTTPException(status_code=409,detail="Log already rejected")
-    else: raise HTTPException(status_code=400,detail="Bad request")
+    Job =await get_jobs(filter_dict={"client_id":token.userId,"_id":ObjectId(log.job_id)})
+    
+    if Job !=None:
+        
+        if (log.client_approved==False) and (log.rejection_reason==None):
+            
+            update_data = LogsUpdate(client_approved=False,rejection_reason=log_rejection.rejection_reason)
+            new_logs= await update_logs_by_id(logs_id=log_id,logs_data=update_data)
+            run_time = datetime.now()+timedelta(days=7)
+            scheduler.add_job(remove_logs,"date",run_date=run_time,args=[log_id],misfire_grace_time=31536000)
+            
+            return APIResponse(status_code=200,data=new_logs,detail="Successfully  approved logs")
+        elif(log.client_approved==False) and (log.rejection_reason!=None): 
+            
+            raise HTTPException(status_code=409,detail="Log already rejected")
+        else:
+            
+            raise HTTPException(status_code=400,detail="Bad request")
 
 @router.post("/post", response_model=APIResponse[LogsOut])
 async def agent_posting_new_logss(log_data: LogsBase = Body(
